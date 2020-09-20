8bc8a727fc0514541afad5b056a6d1e89fdf9499
Allenjjp <2584090148@qq.com>
2020-09-20 18:00:04 +0800

    使用精灵图片

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -10,8 +10,21 @@
       <swiper-slide>
         <img class="w-100" src="../assets/imgs/banner/banner3.jpeg" alt />
       </swiper-slide>
-      <div class="swiper-pagination pagination-home text-right px-3 pb-2" slot="pagination"></div>
+      <div class="swiper-pagination text-right px-3 pb-2" slot="pagination"></div>
     </swiper>
+    <!-- end-of-swiper -->
+    <div class="nav-icons bg-white mt-2 text-center pt-3 text-dark-1">
+      <div class="d-flex flex-wrap">
+        <div class="nav-item mb-3" v-for="n in 10" :key="n">
+          <i class="sprite sprite-news"></i>
+          <div class="py-2">爆料站</div>
+        </div>
+      </div>
+      <div class="bg-light py-2 fs-sm">
+          <i class="sprite sprite-arrow mr-1"></i>
+          <span>收起</span>
+        </div>
+    </div>
   </div>
 </template>
 
@@ -24,7 +37,6 @@ export default {
         pagination: {
           el: ".swiper-pagination",
         },
-        // Some Swiper option/callback...
       },
     };
   },
@@ -39,8 +51,21 @@ export default {
     background-color: map-get($colors, 'white');
     border-radius: 0.1538rem;
     opacity: 1;
+    margin-bottom: -1rem;
     &.swiper-pagination-bullet-active {
       background-color: map-get($colors, 'info');
+    }
+  }
+}
+
+.nav-icons {
+  border-top: 1px solid $border-color;
+  border-bottom: 1px solid $border-color;
+  .nav-item {
+    width: 25%;
+    border-right: 1px solid $border-color;
+    &:nth-child(4n) {
+      border-right: none;
     }
   }
 }
