338048021facd9d4d9b82bc64967c9f17937e929
Allenjjp <2584090148@qq.com>
2020-09-23 22:04:48 +0800

    首页新闻资讯 - 界面展示

--- web/src/components/ListCard.vue
+++ web/src/components/ListCard.vue
@@ -1,43 +1,46 @@
 <template>
-    <m-card :icon="icon" :title="title">
-      <div class="nav jc-between">
-        <div class="nav-item" :class="{active: active === i}" v-for="(category, i) in categories" :key="i" @click="active = i">
-          <div class="nav-link">{{category.name}}</div>
-        </div>
-        
+  <m-card :icon="icon" :title="title">
+    <div class="nav jc-between">
+      <div
+        class="nav-item"
+        :class="{active: active === i}"
+        v-for="(category, i) in categories"
+        :key="i"
+        @click="$refs.list.$swiper.slideTo(i)"
+      >
+        <div class="nav-link">{{category.name}}</div>
       </div>
-      <div class="pt-3">
-        <swiper>
-          <swiper-slide v-for="(category, i) in categories" :key="i">
-            <slot name="items" :category="category">
-                items
-            </slot>
-          </swiper-slide>
-        </swiper>
-      </div>
-    </m-card>
+    </div>
+    <div class="pt-3">
+      <swiper ref="list" @slide-change="() => active = $refs.list.$swiper.realIndex">
+        <swiper-slide v-for="(category, i) in categories" :key="i">
+          <slot name="items" :category="category">items</slot>
+        </swiper-slide>
+      </swiper>
+    </div>
+  </m-card>
 </template>
 
 <script>
 export default {
-    props: {
-        icon: {
-            type: String,
-            required: true
-        },
-        title: {
-            type: String,
-            required: true
-        },
-        categories: {
-            type: Array,
-            required: true
-        }
+  props: {
+    icon: {
+      type: String,
+      required: true,
     },
-    data() {
-        return {
-            active: 0
-        }
+    title: {
+      type: String,
+      required: true,
+    },
+    categories: {
+      type: Array,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      active: 0
     }
-}
+  }
+};
 </script>
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -29,11 +29,11 @@
 
     <m-list-card icon="menu" title="新闻资讯" :categories="newsCats">
       <template #items="{category}">
-        <div class="py-2" v-for="(news, i) in category.newsList" :key="i">
-          <span>[{{news.categoryName}}]</span>
-          <span>|</span>
-          <span>{{news.title}}</span>
-          <span>{{news.date}}</span>
+        <div class="py-2 fs-lg d-flex" v-for="(news, i) in category.newsList" :key="i">
+          <span class="text-info">[{{news.categoryName}}]</span>
+          <span class="px-2">|</span>
+          <span class="flex-1 text-dark-1 text-ellipsis pr-2">{{news.title}}</span>
+          <span class="text-grey-1 fs-sm">{{news.createdAt | date}}</span>
         </div>
       </template>
     </m-list-card>
@@ -41,7 +41,13 @@
 </template>
 
 <script>
+import dayjs from 'dayjs'
 export default {
+  filters: {
+    date(val) {
+      return dayjs(val).format('MM/DD')
+    }
+  },
   name: "Home",
   data() {
     return {
@@ -50,49 +56,17 @@ export default {
           el: ".swiper-pagination",
         },
       },
-      newsCats: [
-        {
-          name: "热门",
-          newsList: new Array(5).fill({}).map((v) => ({
-            categoryName: v + "活动",
-            title: "【探秘新版本，巅峰赢豪礼】活动开启公告",
-            date: "0919",
-          })),
-        },
-        {
-          name: "新闻",
-          newsList: new Array(5).fill({}).map((v) => ({
-            categoryName: v + "新闻",
-            title: "【探秘新版本，巅峰赢豪礼】活动开启公告",
-            date: "0919",
-          })),
-        },
-        {
-          name: "热门",
-          newsList: new Array(5).fill({}).map((v) => ({
-            categoryName: v + "活动",
-            title: "【探秘新版本，巅峰赢豪礼】活动开启公告",
-            date: "0919",
-          })),
-        },
-        {
-          name: "热门",
-          newsList: new Array(5).fill({}).map((v) => ({
-            categoryName: v + "活动",
-            title: "【探秘新版本，巅峰赢豪礼】活动开启公告",
-            date: "0919",
-          })),
-        },
-        {
-          name: "热门",
-          newsList: new Array(5).fill({}).map((v) => ({
-            categoryName: v + "活动",
-            title: "【探秘新版本，巅峰赢豪礼】活动开启公告",
-            date: "0919",
-          })),
-        },
-      ],
+      newsCats: [],
     };
+  },
+  methods: {
+    async fetchNewsCats() {
+      const res = await this.$http.get('news/list')
+      this.newsCats = res.data
+    }
+  },
+  created() {
+    this.fetchNewsCats()
   },
   components: {},
 };
