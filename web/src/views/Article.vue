be278f3a4dedd8a3d4f7b2af31b083c9d28fc1ca
Allenjjp <2584090148@qq.com>
2020-09-28 22:07:21 +0800

    新闻详情页 - 完善

--- web/src/views/Article.vue
+++ web/src/views/Article.vue
@@ -1,15 +1,26 @@
 <template>
   <div class="page-article" v-if="model">
-    <div class="d-flex py-3 px-2">
-      <div class="iconfont icon-back"></div>
-      <strong class="flex-1">
+    <div class="d-flex py-3 px-2 border-bottom">
+      <div class="iconfont icon-back text-blue"></div>
+      <strong class="flex-1 text-blue pl-2">
         {{model.title}}
       </strong>
       <div class="text-grey fs-xs">
         2020-09-28
       </div>
     </div>
-    <div v-html="model.body"></div>
+    <div v-html="model.body" class="px-3 fs-lg article-body"></div>
+    <div class="px-3 border-top py-3">
+      <div class="d-flex ql-align-center">
+        <i class="iconfont icon-menu"></i>
+        <strong class="text-blue fs-lg ml-1">相关资讯</strong>
+      </div>
+      <div class="pt-2">
+        <router-link class="py-1" tag="div" :to="`/articles/${item._id}`" v-for="item in model.related" :key="item._id">
+          {{item.title}}
+        </router-link>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -25,6 +36,12 @@ export default {
       model: null
     }
   },
+  watch: {
+    id: 'fetch'
+    // id() {
+    //   this.fetch()
+    // }
+  },
   methods: {
     async fetch() {
       const res = await this.$http.get(`articles/${this.id}`)
@@ -36,3 +53,23 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+.page-article {
+  .icon-back {
+    font-size: 1.6923rem;
+  }
+  .article-body {
+    img {
+      max-width: 100%;
+      height: auto;
+    }
+    iframe {
+      width: 100%;
+      height: auto;
+    }
+
+  }
+}
+  
+</style>
